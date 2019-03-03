14c08b7f247d78b8394117e36136d3b5164bbab4
salv236 <salvatoresaia@gmail.com>
2019-03-03 19:35:16 +0100

    upload of variable exercise, issue when using type boolean returns string

---
+++ adntechnologies/variables/js/variables.js
@@ -0,0 +1,21 @@
+var word = "salvatore";
+var wholeNumber = 5;
+var decimalNumber = 2.34;
+var status = true;
+var object = [1,2,3,4];
+var definition;
+
+console.log(word);
+console.log(wholeNumber);
+console.log(decimalNumber);
+console.log(status);
+console.log(object);
+console.log(definition);
+
+
+console.log(typeof(word));
+console.log(typeof(wholeNumber));
+console.log(typeof(decimalNumber));
+console.log(typeof(status));
+console.log(typeof(object));
+console.log(typeof(definition));
