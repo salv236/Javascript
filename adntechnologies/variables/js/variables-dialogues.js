ea9248d8f9ee2ac5f54ae8be2ffb439495f42e11
salv236 <salvatoresaia@gmail.com>
2019-03-03 20:05:16 +0100

    inserted htm/js files to provide popups

---
+++ adntechnologies/variables/js/variables-dialogues.js
@@ -0,0 +1,9 @@
+var wholeNumber = 3;
+var decimalNumber = 2.5;
+var word = "salvatore";
+
+alert(wholeNumber + " is an integer");
+alert(decimalNumber + " is a float");
+alert(word + " is an string");
+
+alert(wholeNumber + " is an integer\n" + decimalNumber + " is a float\n" + word + " is an string" );
