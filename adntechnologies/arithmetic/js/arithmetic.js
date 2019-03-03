1b8e04055d654ba41fbe8f6dd217e005c90be1e2
salv236 <salvatoresaia@gmail.com>
2019-03-03 20:17:11 +0100

    inserted js file for different combinations of declaring math operators

---
+++ adntechnologies/arithmetic/js/arithmetic.js
@@ -0,0 +1,11 @@
+//arithmeti operators in performing calculations
+
+var num = 20; //20
+num = num + 10; // 30
+num = num /2; //15
+num++ //16
+num+=6; //22
+num--; //21
+test = (num == 24); //false
+test = (num == 24) && (num >=10); // false +  true =  false
+name = "Damien" + " " + "Fabien"; // Damien Fabien
