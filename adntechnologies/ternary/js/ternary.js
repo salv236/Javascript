c99d0b7c05e3b867379c5ffd83492d38c2ea8c48
salv236 <salvatoresaia@gmail.com>
2019-03-03 20:29:18 +0100

    upload of ternary exercise

---
+++ adntechnologies/ternary/js/ternary.js
@@ -0,0 +1,8 @@
+var num1 = 4;
+var num2 = 12;
+
+var num3 = 15;
+var num4 = 9;
+
+num1 < num2 ? console.log(num1 + " is smaller than " + num2): console.log(num2 + " is smaller than " + num1);
+num3 > num4 ? console.log(num3 + " is greater than " + num4): console.log(num4 + " is greater than " + num3); 
