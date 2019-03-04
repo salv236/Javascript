166fa349a896a6b0182a6aa4925bc626ece33a03
salv236 <salvatoresaia@gmail.com>
2019-03-04 22:16:18 +0100

    upload of eerise for loop odd/even numbers

---
+++ adntechnologies/loops/for/js/js-loops-odd-even.js
@@ -0,0 +1,9 @@
+for (let i=0; i<=15; i++) {
+    if(i%1 == 0) {
+        console.log( i + "= odd") ;
+    } else if(i%2 == 0) {
+        console.log (i + "= even"); 
+    } else {
+        console.log("not odd or even");
+    }
+}
