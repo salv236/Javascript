75c78f43edd231b8813a14de0acbe3a2d38b1520
salv236 <salvatoresaia@gmail.com>
2019-03-05 13:03:06 +0100

    upload of exercise retunr the longest word

---
+++ adntechnologies/functions/js/longest-word.js
@@ -0,0 +1,10 @@
+// function that returns the longest word 
+
+function longestWord(word1, word2) {
+
+    if(word1.length > word2.length || word2.length > word1.length) {
+    return true;
+    } return false;
+}
+
+document.write(longestWord("Salvatore", "Saia"));
