62953de7bcf0f04867c2e21c322b8c9f10fea0b0
salv236 <salvatoresaia@gmail.com>
2019-03-06 11:29:47 +0100

    start of exercise

---
+++ adntechnologies/functions/js/celcius-to-farenheight.js
@@ -0,0 +1,16 @@
+/* write a function that onverts celcius to farenheight */
+
+function convertToCelcius(celcius) {
+
+}
+
+convertToCelcius();
+
+
+/* write a function that onverts farenheight to celcius */
+
+function convertToFarenheight(fheight) {
+
+}
+
+cconvertToFarenheight();
