a159ab5b7f2d67ca46981c1aee955e69d4978fb9
salv236 <salvatoresaia@gmail.com>
2019-03-12 15:55:04 +0100

    added comments explaining why numbers.length-1

--- adntechnologies/objects/js/object-math.js
+++ adntechnologies/objects/js/object-math.js
@@ -3,9 +3,13 @@
 function addArray() {
  
     let total = 0;
-    let numbers = [34, 21, 89, 112];
+    let numbers = [34, 21, 89, 112];  // indexes 0 - 3
 
- for (let i=0; i<=numbers.length-1; i++) {
+ /* start at 0 ,  numbers.length stops at 4 index number 4 does not exist,
+ there are 3 elements hence the reason why numbers.length-1 is needed otherwise loop doesnt work!
+*/
+ 
+    for (let i=0; i<=numbers.length-1; i++) {
      total += numbers[i];
  }
 
