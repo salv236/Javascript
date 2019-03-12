7bc7d92aca44448cb4d95ed197f990a9e765aedb
salv236 <salvatoresaia@gmail.com>
2019-03-12 11:39:56 +0100

    updated the foor loop i<=numbers.length-1

--- adntechnologies/objects/js/object-math.js
+++ adntechnologies/objects/js/object-math.js
@@ -2,11 +2,10 @@
 
 function addArray() {
  
-    let total = "";
+    let total = 0;
     let numbers = [34, 21, 89, 112];
 
- for (let i=0; i<=numbers.length; i++) {
-     //let number = console.log(numbers[i]) ;
+ for (let i=0; i<=numbers.length-1; i++) {
      total += numbers[i];
  }
 
