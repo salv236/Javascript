474968e1035691668a37e8f33906f53054addb73
salv236 <salvatoresaia@gmail.com>
2019-03-16 09:23:34 +0100

    added aditional code examples

--- freecodecamp/ES6/js/varVSlet.js
+++ freecodecamp/ES6/js/varVSlet.js
@@ -16,30 +16,53 @@ let =  limited to a code black cannot be redeclared
 const = values cannot be overridden
 */
 
-function letTest() {
-    let x = 1;
-    if (true) {
-        // restricted to the if statement
-        let x = 2;  // different variable
-      console.log(x);  // 2
-    }
-    console.log(x);  // 1
-  }
+// function letTest() {
+//     let x = 1;
+//     if (true) {
+//         // restricted to the if statement
+//         let x = 2;  // different variable
+//       console.log(x);  // 2
+//     }
+//     console.log(x);  // 1
+//   }
 
-  letTest();
+//   letTest();
 
-  function checkScope() {
-    "use strict";
-      var i = "function scope";
-      if (true) {
-        i = "block scope";
-        console.log("Block scope i is: ", i);
-      }
-      console.log("Function scope i is: ", i);
+/* function inside the for loop will run AFTER the for loop is finished it’s last iteration. Because that’s where 
+you call it, at the bottom inside the console.log. Inside the if statement you just assigned the function to 
+printNumTwo but it won’t run until you don’t call it, it just sits there waiting :)
+The value of ‘i’ is 3 after the for loop has finished and that’s when you call the function, so it makes 
+printNumTwo = 3 (edited) Sorry I used arrow function, it’s the same just less typing :smiley:
+If you don’t use function inside the for loop, just an ‘=‘ it will be 2*/
+var printNumTwo;
+
+for (var i = 0; i < 3; i++) {
+  console.log('value i in for iterations: ' + i)
+  if (i === 2) {
+    console.log('inside if: ' + i);
+    printNumTwo = () => {
       return i;
     }
+  }
+  console.log('end of for: ' + i)
+}
+
+console.log('i value outside of for loop: ' + i)
+
+console.log('final printNumTwo value' + printNumTwo());
+
+//   function checkScope() {
+//     "use strict";
+//       var i = "function scope";
+//       if (true) {
+//         i = "block scope";
+//         console.log("Block scope i is: ", i);
+//       }
+//       console.log("Function scope i is: ", i);
+//       return i;
+//     }
     
-    checkScope();
+//     checkScope();
 
     // CODE INVOLVES HOISTING
 
