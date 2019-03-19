f13ab32ccf6a7cbb424f997fbc79fa6a089e952e
salv236 <salvatoresaia@gmail.com>
2019-03-19 11:11:31 +0100

    anonymous functions with multiple parametres exercise an examples

---
+++ freecodecamp/ES6/js/arrow-functions-with-arguments.js
@@ -0,0 +1,14 @@
+// EXAMPLES
+
+// doubles input value and returns it, multiple parametres can be passed
+const doubler = (item) => item * 2;
+
+//EXERCISE
+
+"use strict";
+
+const myConcat = (arr1, arr2) => arr1.concat(arr2); {
+
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
