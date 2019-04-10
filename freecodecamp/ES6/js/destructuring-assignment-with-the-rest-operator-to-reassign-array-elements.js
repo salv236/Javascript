c7b9710753489fd73feb8fcc460d721cc0752c49
salv236 <salvatoresaia@gmail.com>
2019-04-10 14:17:25 +0200

    issue with destructing first 2 values and remainder with rest

---
+++ freecodecamp/ES6/js/destructuring-assignment-with-the-rest-operator-to-reassign-array-elements.js
@@ -0,0 +1,20 @@
+//EXAMPLE
+
+const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+console.log(a, b); // 1, 2
+console.log(arr); // [3, 4, 5, 7]
+
+
+//EXERCISE
+
+const source = [1,2,3,4,5,6,7,8,9,10];
+function removeFirstTwo(list) {
+  "use strict";
+  // change code below this line
+  arr[a,b, ...arr] = list; // change this
+  // change code above this line
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10]
