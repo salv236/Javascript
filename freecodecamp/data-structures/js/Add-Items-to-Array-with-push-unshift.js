dee7171d891211493e8b6d746183f5fe7f86a85d
salv236 <salvatoresaia@gmail.com>
2019-04-16 12:10:47 +0200

    added completed extercise push unshift

---
+++ freecodecamp/data-structures/js/Add-Items-to-Array-with-push-unshift.js
@@ -0,0 +1,24 @@
+// EXAMPLE
+let twentyThree = 'XXIII';
+let romanNumerals = ['XXI', 'XXII'];
+
+romanNumerals.unshift('XIX', 'XX');
+// now equals ['XIX', 'XX', 'XXI', 'XXII']
+
+romanNumerals.push(twentyThree);
+// now equals ['XIX', 'XX', 'XXI', 'XXII', 'XXIII']
+
+
+//EXERCISE
+
+function mixedNumbers(arr) {
+    // change code below this line
+     arr.unshift('I', 2, 'three');
+     arr.push(7, 'VIII', 9);
+    // change code above this line
+    return arr;
+  }
+  
+  // do not change code below this line
+  console.log(mixedNumbers(['IV', 5, 'six']));
+
