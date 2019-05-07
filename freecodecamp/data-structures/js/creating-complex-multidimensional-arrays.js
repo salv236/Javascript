62ebdd428f1edc0cef3df84e2027c52201dcc3c3
salv236 <salvatoresaia@gmail.com>
2019-05-07 15:33:54 +0200

    update example with commets for the aray outputs for each deep level

--- freecodecamp/data-structures/js/creating-complex-multidimensional-arrays.js
+++ freecodecamp/data-structures/js/creating-complex-multidimensional-arrays.js
@@ -1,13 +1,16 @@
 //EXAMPLE
 
 let nestedArray = [ // top, or first level - the outer most array
-    ['deep'], // an array within an array, 2 levels of depth
-    // 2 arrays nested 3 levels deep
-    [['deeper'], ['deeper'] ],
-    // 2 arrays nested 4 levels deep
-    [[ ['deepest'], ['deepest'] ],
-    // an array nested 5 levels deep
-      [[['deepest-est?'] ]]
+    ['deep'], // an array within an array, 2 levels of depth [0]
+    [
+      ['deeper'], ['deeper'] // 2 arrays nested 3 levels deep [1][0], [1][1]
+    ],
+    
+    [[['deepest'], ['deepest'] // 2 arrays nested 4 levels deep [2][0][0], [2][0][1]
+      ],
+      [[['deepest-est?'] // an array nested 5 levels deep [2][1][0][0]
+        ]
+      ]
     ]
   ];
 
