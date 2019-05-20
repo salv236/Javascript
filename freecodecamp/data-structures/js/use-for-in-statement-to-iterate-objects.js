a146ab1e8195fe6c0735e9829a38b0978fc27138
salv236 <salvatoresaia@gmail.com>
2019-05-20 17:00:01 +0200

    inserted for in loop still issues with returning all objects that are true

--- freecodecamp/data-structures/js/use-for-in-statement-to-iterate-objects.js
+++ freecodecamp/data-structures/js/use-for-in-statement-to-iterate-objects.js
@@ -1,45 +1,34 @@
- // EXAMPLE
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
 
- for (let user in users) {
-    console.log(user);
-  };
-  
-  // logs:
-  Alan
-  Jeff
-  Sarah
-  Ryan
- 
- //EXERCISE
-
- let users = {
-    Alan: {
-      age: 27,
-      online: false
-    },
-    Jeff: {
-      age: 32,
-      online: true
-    },
-    Sarah: {
-      age: 48,
-      online: false
-    },
-    Ryan: {
-      age: 19,
-      online: true
-    }
-  };
-  
-  function countOnline(obj) {
+function countOnline(obj) {
     // change code below this line
-    for(user in users) {
-       if (users.hasOwnProperty(obj) === true) {
-         return obj;
-       }
-    } 
+    for(obj in users) {
+      console.log(users[obj]);
+      // if the online property === true output the value
+     if(users[obj].online === true){
+       return users[obj];
+       //console.log(obj);
+     } 
   
     // change code above this line
   }
-  
+
+}
   console.log(countOnline(users));
