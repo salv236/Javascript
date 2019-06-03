5d6f97efc44788709796365bec4c823229f9ddf8
salv236 <salvatoresaia@gmail.com>
2019-06-03 13:41:48 +0200

    added solution to avoid infinite loop

---
+++ freecodecamp/debugging/js/Prevent-Infinite-Loops-with-a-Valid-Terminal-Condition.js
@@ -0,0 +1,15 @@
+//EXAMPLE
+
+function loopy() {
+    while(true) {
+      console.log("Hello, world!");
+    }
+  }
+
+//EXERCISE 
+
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
